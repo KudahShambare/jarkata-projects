786b9ebccfe1db3fd0a5f8812a2d68a2558f9a53
Kudakwashe Shambare <70160442+KudahShambare@users.noreply.github.com>
2025-12-25 01:04:00 +0200

    initiall jsp template created

---
+++ student-crud/src/main/java/com/kuda/servlet/Hello.java
@@ -0,0 +1,19 @@
+package com.kuda.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello")
+public class Hello extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
+ request.getRequestDispatcher("/index.jsp").forward(request, response);    }
+}
